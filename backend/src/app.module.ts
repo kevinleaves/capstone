cd5352d68e826238204ef1acf5130a91d8bac2bf
Kevin Le <iamkevinvle@gmail.com>
2025-02-25 13:54:48 -0800

    add configModule & configService to load development/testing environments

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,43 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CasesModule } from './cases/cases.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Case } from './cases/cases.entity';
+import { DataSource } from 'typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/users.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [Case, User],
+    //   synchronize: true,
+    // }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [Case, User],
+        };
+      },
+    }),
+    CasesModule,
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
